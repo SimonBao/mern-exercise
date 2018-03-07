11201970a86aa31b1d41f3715e018004435fba97
SimonBao <Simonbao.sb@gmail.com>
2018-03-07 19:49:21 +0000

    Added semi colons to end statements;

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import config, { nodeEnv, logStars } from './config'
+import config, { nodeEnv, logStars } from './config';
 import apiRouter from './api';
-import express from 'express'
+import express from 'express';
 const server = express();
 
 server.set('view engine', 'ejs');
@@ -9,12 +9,12 @@ server.get('/', (req, res) => {
     res.render('index', {
         content: '<h1>    EJS BODY    </h1>'
     });
-})
+});
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
 server.listen(config.port, () => {
     console.log('Express listening on port: ', config.port);
-})
+});
 
