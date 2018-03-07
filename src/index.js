97184ae1e52f40850fbdde4798bdfdd8cc242c6c
SimonBao <Simonbao.sb@gmail.com>
2018-03-07 20:21:56 +0000

    Fixed 'react-dom' import.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    React.createElement('h2', null, 'Hello React'),
+    React.createElement("h2", null, "HELLO REACT"),
     document.getElementById('root')
 );
