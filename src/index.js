ef00a77eacd09928c6c6b3e60a102c0f63ba3621
SimonBao <Simonbao.sb@gmail.com>
2018-03-07 21:30:45 +0000

    Render App and Header components in React.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const color = Math.random() > 0.5 ? 'green' : 'red';
 
+const Header = ({ message }) => {
+    return (
+        <div className="Header">
+            <h2 className='text-center'>
+                {message}
+            </h2>
+        </div>
+    );
+};
+
+
+const App = (props) => {
+    return (
+        <div className="App">
+            <Header message="Hello React" />
+            <div>
+            </div>
+        </div>
+    );
+};
+
+App.defaultProps = {
+    headerMessage: 'hello'
+};
+
 ReactDOM.render(
-    <h2 style={{color}}>Hello -- {Math.random()}</h2>,
+    <App headerMessage={4} />,
     document.getElementById('root')
 );
